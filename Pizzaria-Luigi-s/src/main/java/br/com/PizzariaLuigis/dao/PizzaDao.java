e8a7f5ac84b2db183d1bc5d154d49caaf49684df
Guilherme <pepe.ks.rx.1@gmail.com>
2024-04-27 15:03:00 -0300

    Mudanças na landing page e criação da camada model e dao da Pizza

---
+++ Pizzaria-Luigi-s/src/main/java/br/com/PizzariaLuigis/dao/PizzaDao.java
@@ -0,0 +1,36 @@
+package br.com.PizzariaLuigis.dao;
+import br.com.PizzariaLuigis.model.Pizza;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
+public class PizzaDao {
+
+    public void createPizza(Pizza pizza){
+
+        String SQL = "INSERT INTO PIZZA (NOME, PRECO, DESCRICAO ) VALUES (?,?,?)";
+
+        try {
+            Connection con = DriverManager.getConnection("jdbc:h2:~/test", "sa","sa");
+
+            System.out.println("success in database connection");
+
+            PreparedStatement preparedStatement = con.prepareStatement(SQL);
+
+            preparedStatement.setString(1,pizza.getNome());
+            preparedStatement.setDouble(2,pizza.getPreco());
+            preparedStatement.setString(3,pizza.getDescricao());
+
+            preparedStatement.execute();
+
+            System.out.println("success in insert pizza");
+
+            con.close();
+        }
+        catch (Exception e){
+            System.out.println("fail in database connection");
+        }
+    }
+
+}
--- Pizzaria-Luigi-s/src/main/java/br/com/PizzariaLuigis/model/Pizza.java
+++ Pizzaria-Luigi-s/src/main/java/br/com/PizzariaLuigis/model/Pizza.java
@@ -4,4 +4,28 @@ public class Pizza {
     private String nome;
     private double preco;
     private String descricao;
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 }
